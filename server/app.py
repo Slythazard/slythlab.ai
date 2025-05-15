ce4bc441312ae67865be5058805a9311ba75e636
Slythazard <shreshthkatyayan25@gmail.com>
2025-05-16 00:51:28 +0530

    installed pillow

--- server/app.py
+++ server/app.py
@@ -10,7 +10,7 @@ load_dotenv()
 CLIENT_PORT = os.getenv("CLIENT_PORT")
 
 app=Flask(__name__)
-CORS(app, resources ={r"/api/*": {"origins":[ f"http://localhost:{CLIENT_PORT}","https://slythlab-ai.onrender.com"]}})
+CORS(app, resources ={r"/api/*": {"origins":[ f"http://localhost:{CLIENT_PORT}","https://slythlab-ai.onrender.com","https://slythlab-ai.vercel.app/"]}})
 
 app.register_blueprint(api,url_prefix='/api')
 
@@ -22,6 +22,6 @@ def home():
     return jsonify(data)
 
 if __name__=="__main__":
-    port = int(os.getenv('PORT'))
+    port = int(os.environ.get('PORT'))
     print(port)
     app.run(host='0.0.0.0',port=port)
